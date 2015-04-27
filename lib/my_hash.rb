183a186b0fe14886e5969ef48f13870dd7d76afc
bob ellis & cory spitzer <coryspitzer@gmail.com>
2015-04-27 10:01:09 -0700

    Define initialize, fetch, and store to pass test to retrieve value by key.

---
+++ lib/my_hash.rb
@@ -0,0 +1,19 @@
+class MyHash
+  define_method(:initialize) do |key='', value=''|
+    @key = key
+    @value = value
+    @keys = []
+    @values = []
+  end
+
+  define_method(:store) do |key='', value=''|
+    @values.push(value)
+    @keys.push(key)
+  end
+
+  define_method(:fetch) do |key|
+    index = @keys.index(key)
+    @values.at(index)
+  end
+
+end
