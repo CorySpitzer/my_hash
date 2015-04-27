7d967c764479e301611b2c68b70294b84723f849
bob ellis & cory spitzer <coryspitzer@gmail.com>
2015-04-27 10:48:27 -0700

    Actually test whether a key exists

--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -2,19 +2,26 @@ require('rspec')
 require('my_hash')
 
 describe(MyHash) do
-  describe("#fetch") do
-    it("retreives a stored value by its key") do
+  describe('#fetch') do
+    it('retreives a stored value by its key') do
       test_hash = MyHash.new()
-      test_hash.store("kitten", "cute")
-      expect(test_hash.fetch("kitten")).to(eq("cute"))
+      test_hash.store('kitten', 'cute')
+      expect(test_hash.fetch('kitten')).to(eq('cute'))
     end
   end
 
-  describe("#has_key?") do |key|
-    it("returns true if it has the key specified") do
+  describe('#has_key?') do |key|
+    it('returns true if it has the key specified') do
       test_hash = MyHash.new()
-      test_hash.store("kitten","cute")
-      test_hash.has_key?("kitten") == true
+      test_hash.store('kitten','cute')
+      expect(test_hash.has_key?('kitten')).to(eq(true))
+    end
+  end
+
+  describe('#has_key?') do |key|
+    it('returns false if does not have the key specified') do
+      test_hash = MyHash.new()
+      expect(test_hash.has_key?('a')).to(eq(false))
     end
   end
 
